801a1946ebc8e9dfc9eb106ea9942b9c2c51cbe3
mukunddkumar <mukunddkumar@gmail.com>
2024-02-28 19:33:43 +0530

    bug fix and doxument
    fixed the bug and uploaded the document about bugs

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=["POST"])
+def index():
+    notes = []
+    note = request.form.get("note")
+    if note:
+        notes.append(note)
+    return render_template("home.html", notes=notes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
